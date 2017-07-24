04350e495ebe3e11ae1dd63585c4aa1655ad1a2d
mxdavis <md01@me.com>
2017-07-24 04:33:20 +0300

    add navbar with styling from uikit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router, 
   Route, 
+  Switch,
   Link
-} from 'react-dom-router'
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="navbar">
-          <a href="/login">Sign In</a>
+      <Router>
+        <div className="App">
+          <nav className="uk-navbar">
+            <div className="uk-navbar-flip">
+              <ul className="uk-navbar-nav">
+                <li><a href="/login">Sign In</a></li>
+              </ul>
+            </div>
+          </nav>  
+          <Switch>
+            <Route exact path = "/" />
+            {/*<Route exact path = "/signup" component={Signup}/>*/}
+            <Route  />
+          </Switch>
         </div>
-      </div>
+      </Router>
     );
   }
 }
