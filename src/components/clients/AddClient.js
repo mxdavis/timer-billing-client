1307afc9d4f469d31d58de599f27986db288499b
mxdavis <md01@me.com>
2017-07-26 00:36:35 +0300

    add client form with connect props and dispatch but not working

---
+++ src/components/clients/AddClient.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux';
+import { addClient } from '../../redux/actions/addClient'
+
+class AddClient extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      name: '',
+      email: ''
+    }
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    debugger;
+    const { addClient, history } = this.props
+    addClient(this.state);
+    history.push('/tasks');
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+ 
+  render() {
+    return (
+      <div>
+        <form className="uk-form" onSubmit={this.handleOnSubmit}>
+          <fieldset>
+              <legend>Add Client</legend>
+              <div className="uk-form-row">
+                <legend>Client Name</legend>
+                <input
+                type="text"
+                placeholder="Client Name"
+                name="name"
+                onChange={this.handleOnChange} />
+              </div>
+              <div className="uk-form-row">
+                <legend>Client E-mail</legend>
+                <input
+                type="text"
+                placeholder="Client E-mail"
+                name="email"
+                onChange={this.handleOnChange} />
+              </div>
+              <input
+            type="submit"
+            value="Add Client" />
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+};
+
+const mapStateToProps = state => {
+  return { 
+    name: state.client.name,
+    email: state.client.email
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    { 
+    addClient
+    }
+  , dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddClient);
