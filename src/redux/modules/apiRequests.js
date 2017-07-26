0b5788a6b70ea0736b4cf02f715fc6190486a052
mxdavis <md01@me.com>
2017-07-26 04:43:17 +0300

    get now works in the module able to be used anywhere in the client

--- src/redux/modules/apiRequests.js
+++ src/redux/modules/apiRequests.js
@@ -6,15 +6,25 @@ const api_url = 'http://localhost:3000/api/v1/users/1/'
 
 export default {
 
-   post(url, body) {
+  get(url){
     return fetch(api_url + url, {
-      method: 'POST',
+      method: 'GET',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body)
-    }).then(response => (response.json()))
-   }
+      }
+    }).then(response => response.json())
+  },
+
+  post(url, body) {
+  return fetch(api_url + url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body)
+  }).then(response => (response.json()))
+  }
 
 }
