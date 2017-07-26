49cafa1fbd254bdcdb90d538b39ef7d2044b06e5
mxdavis <md01@me.com>
2017-07-26 04:45:37 +0300

    use the get from the apiRequest

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
+
+import apiRequest from '../../redux/modules/apiRequests'
+
 require('isomorphic-fetch');
 
 export default class Tasks extends Component {
   componentDidMount() {
-    const headers =  {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    }
-    const link = 'http://localhost:3000/' || 'https://bill-timer-client.herokuapp.com/'
-    fetch('http://localhost:3000/api/v1/users/1/unbilled_tasks', {
-      method: 'GET',
-      headers: headers
-    })
-      .then(response => response.json())
-      .then(data => console.log(data))
+    apiRequest.get('unbilled_tasks').then(data => console.log(data))
   }
  
   render() {
