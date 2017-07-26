62e6b0df8a2a6899f11487e273883aeb94364aaa
mxdavis <md01@me.com>
2017-07-27 02:56:53 +0300

    create fetching data reducer for billed and unbilled tasks

---
+++ src/redux/reducers/fetchingData.js
@@ -0,0 +1,17 @@
+export default (state = {
+  fetching: []
+}, action) => {
+  switch(action.type) {
+    case "STOP_FETCHING_BILLED_TASKS":
+      return state.fetching.filter((i) => i !== billed_tasks)
+    case "START_FETCHING_BILLED_TASKS":
+      return state.fetching.concat(billed_tasks)
+    case "STOP_FETCHING_UNBILLED_TASKS":
+      return state.fetching.filter((i) => i !== unbilled_tasks)
+    case "START_FETCHING_UNBILLED_TASKS":
+      return state.fetching.concat(unbilled_tasks)
+    default: 
+      return state;
+  }
+};
+
