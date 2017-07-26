003f6eeec752784d788e67f93d1e03347169f35e
mxdavis <md01@me.com>
2017-07-26 23:45:35 +0300

    update reducer correctly so it saves to store properly

--- src/redux/reducers/tasks/tasks.js
+++ src/redux/reducers/tasks/tasks.js
@@ -1,14 +1,18 @@
 export default function tasks(state = {
-  unbilled_tasks: [], 
-  billed_tasks: []
+    unbilled_tasks: [], 
+    billed_tasks: []
 }, action) {
   switch (action.type) {
     case 'UPDATE_UNBILLED_TASKS':
       const unbilled_tasks = action.tasks.filter(t => t != null)
-      return state.unbilled_tasks = Object.assign({}, unbilled_tasks)
+      return Object.assign({}, state.tasks, {
+        unbilled_tasks: unbilled_tasks
+      });
      case 'UPDATE_BILLED_TASKS':
-       const billed_tasks = action.tasks.filter(t => t != null)
-       return state.billed_tasks = Object.assign({}, billed_tasks)
+      const billed_tasks = action.tasks.filter(t => t != null)
+      return Object.assign({}, state.tasks, {
+        billed_tasks: billed_tasks
+      });
     default:
       return state;
   }
