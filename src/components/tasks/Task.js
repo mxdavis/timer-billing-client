325b481eb99b1913908eac4e30fc6612e02f14ba
mxdavis <md01@me.com>
2017-07-26 04:43:58 +0300

    create a single task container

---
+++ src/components/tasks/Task.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Task extends Component {
+  render(props){
+    return(
+        <div >
+          This is what a task is going to look like
+        </div>
+    )
+  }
+}
