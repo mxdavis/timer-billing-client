c89affad5abcd5807ec84b7684ad8c61bcb75025
mxdavis <md01@me.com>
2017-07-26 05:24:19 +0300

    separate internal import from redux react import

--- src/redux/reducers/clients/addClient.js
+++ src/redux/reducers/clients/addClient.js
@@ -4,6 +4,7 @@ export default function addClient(state = {
   switch (action.type) {
     case 'ADD_CLIENT':
       Object.assign({}, state.client)
+      break
     default:
       return state;
   }
