7ee44bee5ccf68757ceca99f47ca6b378dad4fe8
mxdavis <md01@me.com>
2017-07-27 03:04:29 +0300

    add quotes around billed and unbilled tasks

--- src/redux/reducers/fetchingData.js
+++ src/redux/reducers/fetchingData.js
@@ -3,13 +3,13 @@ export default (state = {
 }, action) => {
   switch(action.type) {
     case "STOP_FETCHING_BILLED_TASKS":
-      return state.fetching.filter((i) => i !== billed_tasks)
+      return state.fetching.filter((i) => i !== "billed_tasks")
     case "START_FETCHING_BILLED_TASKS":
-      return state.fetching.concat(billed_tasks)
+      return state.fetching.concat("billed_tasks")
     case "STOP_FETCHING_UNBILLED_TASKS":
-      return state.fetching.filter((i) => i !== unbilled_tasks)
+      return state.fetching.filter((i) => i !== "unbilled_tasks")
     case "START_FETCHING_UNBILLED_TASKS":
-      return state.fetching.concat(unbilled_tasks)
+      return state.fetching.concat("unbilled_tasks")
     default: 
       return state;
   }
