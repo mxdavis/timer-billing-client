4651ec72fb23cc1fcd3c340da5c186384ad9e756
mxdavis <md01@me.com>
2017-07-27 15:58:24 +0300

    currently changing text in the dom, but the state is not available at the last re render so i cant list all the tasks

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -4,20 +4,41 @@ import { bindActionCreators } from 'redux';
 
 import { tasks } from '../../redux/actions/tasks/tasks'
 import apiRequest from '../../redux/modules/apiRequests'
+import {startFetchingData, stopFetchingData} from '../../redux/actions/fetchingData'
 import Task from './Task'
 
 require('isomorphic-fetch');
 
 class Tasks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      fetching: ["unbilled", "billed"], 
+      tasks: {
+        billed_tasks: this.state.billed_tasks,
+        unbilled_tasks: this.state.unbilled_tasks
+    } };
+  }
 
   getTasks = (tasks_type) => {
+    const task_type_to_cap = `${tasks_type}_tasks`.toUpperCase()
     apiRequest.get(`${tasks_type}_tasks`)
       .then(tasks => this.props.tasks(tasks, tasks_type))
-      .then(tasks => this.showTasks(tasks))
+      .then(this.setState({
+        fetching: this.state.fetching.filter((i) => i !== tasks_type)
+      }))
+      .then(this.showTasks(tasks_type))
   }
     
-  showTasks = (tasks) => {
-    tasks.map(task => <Task props={task} />)
+  showTasks = (tasks, state) => {
+    debugger
+    if (this.tasks){
+      return <div> I have loaded</div>
+    }
+   
+      
+    // return state ? <div>I have state</div> : <div> state is undefined</div>
+    // this.state[tasks].map(task => <Task props={task} />) 
   }
 
   componentDidMount() {
@@ -29,30 +50,26 @@ class Tasks extends Component {
   render() {
     return (
       <div>
-        <h1 class="uk-heading-primary">Welcome User</h1>
-        <h1 class="uk-heading-line uk-text-center"><span>Here are your tasks waiting to be billed:</span></h1>
-        
-        {/*{this.showTasks(this.state.tasks.unbilled_tasks)}*/}
-        <h1 class="uk-heading-line uk-text-center"><span>Here are your tasks that were already billed:</span></h1>
-        {/*{this.showTasks(this.state.tasks.billed_tasks)}*/}
+        <h1 className="uk-heading-primary">Welcome User</h1>
+        <h1 className="uk-heading-line uk-text-center"><span>Here are your tasks waiting to be billed:</span></h1>
+        {(this.state.fetching.length > 0 && this.state.fetching.indexOf("billed") > -1) ? <div className="uk-spinner">I am loading</div> : this.showTasks("unbilled", this.state)}
+        <h1 className="uk-heading-line uk-text-center"><span>Here are your tasks that were already billed:</span></h1>
+        {(this.state.fetching.length > 0 && this.state.fetching.indexOf("billed") > -1) ? <div className="uk-spinner">I am loading</div> : this.showTasks("billed", this.state)}
       </div>
     )
   }
 }
 
 // will i need this to pass into task container?
-// const mapStateToProps = state => {
-//   return { 
-//     tasks: {
-//       billed_tasks: state.billed_tasks,
-//       unbilled_tasks: state.unbilled_tasks
-//     }
-//   }
-// }
+const mapStateToProps = state => {
+  return { 
+    tasks: state.tasks
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return bindActionCreators(
-    { tasks }, dispatch);
+    { tasks, startFetchingData, stopFetchingData }, dispatch);
 };
 
-export default connect(null, mapDispatchToProps)(Tasks);
+export default connect(mapStateToProps, mapDispatchToProps)(Tasks);
