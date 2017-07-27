7729cf5478ebe656f46f9fab2bccf984b1b43042
mxdavis <md01@me.com>
2017-07-28 02:31:00 +0300

    remove debugger

--- src/redux/actions/tasks/tasks.js
+++ src/redux/actions/tasks/tasks.js
@@ -2,7 +2,6 @@ import { stopFetchingData, startFetchingData } from '../fetchingData';
 import apiRequest from '../../modules/apiRequests'
 
 export function tasks(tasks, billed_status){
-  debugger
   if (billed_status === 'unbilled'){
     return {
       type: 'UPDATE_UNBILLED_TASKS',
@@ -21,7 +20,6 @@ export function fetchTasks(taskType) {
     dispatch(startFetchingData(taskType.toUpperCase()));
     apiRequest.get(`${taskType}_tasks`)
      .then(data => {
-       debugger
         dispatch(tasks(data, taskType))
         dispatch(stopFetchingData(taskType.toUpperCase()));
       })
