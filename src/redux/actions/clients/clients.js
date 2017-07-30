8188392b21a153a2edf98415fe03de697ec37f6c
mxdavis <md01@me.com>
2017-07-30 03:00:07 +0300

    add fetching data function and update all clients

--- src/redux/actions/clients/clients.js
+++ src/redux/actions/clients/clients.js
@@ -1,6 +1,23 @@
+import { stopFetchingData, startFetchingData } from '../fetchingData';
+import apiRequest from '../../modules/apiRequests'
+
 export function clients(clients){
   return {
     type: 'UPDATE_CLIENTS',
     clients
-  };
-};
+  }
+}
+
+export function fetchClients() {
+  return dispatch => {
+    dispatch(startFetchingData("CLIENTS"))
+    apiRequest.get('clients')
+     .then(data => {
+        dispatch(clients(data))
+        dispatch(stopFetchingData("CLIENTS"))
+      })
+    .catch(error => {
+        console.log(error)
+    })
+  }
+}
