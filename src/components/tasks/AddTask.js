79c50c7ab35c9055ed298f17ecf10a7263ea0eab
mxdavis <md01@me.com>
2017-07-30 07:56:59 +0300

    created label and value for form  (client)

--- src/components/tasks/AddTask.js
+++ src/components/tasks/AddTask.js
@@ -1,6 +1,57 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux';
+import Select from 'react-select';
+
+import selectForms from '../../redux/modules/selectForms'
+import { clients, fetchClients } from '../../redux/actions/clients/clients'
+
+Object.prototype.isEmpty = function() {
+    for(var key in this) {
+        if(this.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
 
 class AddTask extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      clientData: [],
+      clientArray: []
+    }
+  }
+  
+  convertClientstoDropdown() {
+    // if (!this.state.clients){
+    //     fetchClients
+    //     //what happens if no one has any clients yet
+    //     // if (!this.state.clients && !this.state.fetchingData.includes("CLIENTS")){
+    //     //   //redirect to add client page
+    //     //   return "you need to add clients"
+    //     // }
+    //   }
+    debugger
+    if (!this.props.clientArray.isEmpty() && this.state.clientData.isEmpty()){
+    const clientData =  this.props.clientArray.clients.map(client => {
+        var rObj = {};
+        rObj["value"] = client.id;
+        rObj["label"] = client.name
+        return rObj;
+      })   
+    this.setState({clientData: clientData})
+    }
+  }
+
+  componentDidMount() {
+    this.props.fetchClients()
+  }
+
+  componentDidUpdate(){
+    this.convertClientstoDropdown()
+  }
  
   render() {
     return (
@@ -29,4 +80,16 @@ class AddTask extends Component {
   }
 };
 
-export default AddTask;
+const mapStateToProps = state => {
+  return { 
+    clientArray: state.clients,
+    fetchingData: state.fetchingData
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    { fetchClients }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddTask);
