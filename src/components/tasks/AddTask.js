32596fb42ae656b169ca3d8b190278658bdf4533
mxdavis <md01@me.com>
2017-07-30 13:46:36 +0300

    select form shows clients, and when changes changes the value but wont update get state, im going to try another way to do it

--- src/components/tasks/AddTask.js
+++ src/components/tasks/AddTask.js
@@ -20,9 +20,54 @@ class AddTask extends Component {
     super(props)
     this.state = {
       clientData: [],
-      clientArray: []
+      clientArray: [],
+      client: '',
+      selectValue: ''
     }
   }
+
+	getDefaultProps(){
+		return {
+			label: 'Clients:',
+			searchable: true,
+		};
+	}
+
+	getInitialState() {
+		return {
+			disabled: false,
+			searchable: this.props.searchable,
+			selectValue: 'select client',
+			clearable: true,
+		};
+	}
+
+	switchClient(e) {
+		var newClient = e.target.value;
+		console.log('Client changed to ' + newClient);
+		this.setState({
+			client: newClient,
+			selectValue: null
+		});
+	}
+
+	updateValue (newValue) {
+		console.log('Client changed to ' + newValue);
+    debugger
+		this.setState({
+			selectValue: newValue
+		});
+	}
+
+	focusClientSelect () {
+		this.refs.clientSelect.focus();
+	}
+
+	toggleCheckbox (e) {
+		let newState = {};
+		newState[e.target.name] = e.target.checked;
+		this.setState(newState);
+	}
   
   convertClientstoDropdown() {
     if (!this.props.clientArray.isEmpty() && this.state.clientData.isEmpty()){
@@ -45,8 +90,43 @@ class AddTask extends Component {
   }
  
   render() {
+     var options = this.state.clientData;
     return (
       <div>
+
+			<div className="section">
+				<h3 className="section-heading">{this.props.label}</h3>
+				<Select ref="clientSelect" autofocus options={options} simpleValue clearable={this.state.clearable} name="selected-client" disabled={this.state.disabled} value={this.state.selectValue} onChange={this.updateValue} searchable={this.state.searchable} />
+
+				<div style={{ marginTop: 14 }}>
+					<button type="button" onClick={this.focusClientSelect}>Focus Select</button>
+					<label className="checkbox" style={{ marginLeft: 10 }}>
+						<input type="checkbox" className="checkbox-control" name="searchable" checked={this.state.searchable} onChange={this.toggleCheckbox}/>
+						<span className="checkbox-label">Searchable</span>
+					</label>
+					<label className="checkbox" style={{ marginLeft: 10 }}>
+						<input type="checkbox" className="checkbox-control" name="disabled" checked={this.state.disabled} onChange={this.toggleCheckbox}/>
+						<span className="checkbox-label">Disabled</span>
+					</label>
+					<label className="checkbox" style={{ marginLeft: 10 }}>
+						<input type="checkbox" className="checkbox-control" name="clearable" checked={this.state.clearable} onChange={this.toggleCheckbox}/>
+						<span className="checkbox-label">Clearable</span>
+					</label>
+				</div>
+				{/*<div className="checkbox-list">
+					<label className="checkbox">
+						<input type="radio" className="checkbox-control" checked={this.state.country === 'AU'} value="AU" onChange={this.switchCountry}/>
+						<span className="checkbox-label">Australia</span>
+					</label>
+					<label className="checkbox">
+						<input type="radio" className="checkbox-control" checked={this.state.country === 'US'} value="US" onChange={this.switchCountry}/>
+						<span className="checkbox-label">United States</span>
+					</label>
+				</div>*/}
+			</div>
+
+
+
         HELLO
         <form class="uk-form">
           <fieldset>
@@ -74,7 +154,7 @@ class AddTask extends Component {
 const mapStateToProps = state => {
   return { 
     clientArray: state.clients,
-    fetchingData: state.fetchingData
+    fetchingData: state.fetchingData,
   }
 }
 
