8807c6c2ab4dd80773faafa80d8723d015655150
mxdavis <md01@me.com>
2017-07-30 04:28:30 +0300

    Add $ to bill rate

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -3,6 +3,6 @@ import React from 'react'
 const Project = ({ project: { id, name, bill_rate}}) => 
   <tr key={id}>
     <td className="uk-text-truncate">{name}</td>
-    <td className="uk-text-truncate">{bill_rate}</td>
+    <td className="uk-text-truncate">${bill_rate}</td>
   </tr>
 export default Project
