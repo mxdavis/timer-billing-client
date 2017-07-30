c6dd59200681a52b4e0260fd09b3300dc835fc1c
mxdavis <md01@me.com>
2017-07-30 04:19:47 +0300

    Show different message if projects exist or if they dont

--- src/components/clients/Client.js
+++ src/components/clients/Client.js
@@ -2,22 +2,28 @@ import React from 'react'
 
 import Project from '../projects/Project'
 
+const currentProjects = (projects) => {
+  return projects.length > 0 ?
+    <div className="uk-overflow-auto">
+      <table className="uk-table uk-table-hover uk-table-middle uk-table-divider uk-table-striped uk-table-responsive">
+        <thead>
+          <tr>
+            <th className="uk-table-expand">Project Name</th>
+            <th className="uk-table-expand">Bill Rate</th>
+          </tr>
+        </thead>
+      <tbody>
+          {projects.map(p => <Project project={p} />)}
+      </tbody>
+    </table>
+  </div> :
+  <h3>Please add a new project!</h3>
+}
+
 const Client = ({ client: { id, name, email, projects}}) => 
   <div key={id} className="uk-width-5-10 uk-container-center">
-    <h2 className="uk-heading-line uk-text-center"><span>{name} <h3>{email}</h3></span></h2>
-      <div className="uk-overflow-auto">
-        <table className="uk-table uk-table-hover uk-table-middle uk-table-divider uk-table-striped uk-table-responsive">
-          <thead>
-            <tr>
-              <th className="uk-table-expand">Project Name</th>
-              <th className="uk-table-expand">Bill Rate</th>
-            </tr>
-          </thead>
-        <tbody>
-          {projects.map(p => <Project project={p} />)}
-        </tbody>
-      </table>
-    </div>
-  </div>
+      <h2 className="uk-heading-line uk-text-center"><span>{name} <h3>{email}</h3></span></h2>
+      {currentProjects(projects)}
+  </div> 
 
 export default Client
