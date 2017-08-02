64d29d5bded67a4a76c2b6c0ee5bfcb1f418bba4
mxdavis <md01@me.com>
2017-08-03 00:41:59 +0300

    when no tasks

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -15,9 +15,9 @@ class Clients extends Component {
   showClient = () => {
     return this.props.clientArray.clients !== undefined && this.props.clientArray.clients !== [] ? 
     <div>
-     {this.props.clientArray.clients.map(client => <Client client={client}/>)}
+     {this.props.clientArray.clients.map(client => <div key={client.id}className="uk-overflow-auto"><Client client={client}/></div>)}
     </div> : 
-    this.props.clientArray.clients === undefined? 
+    this.props.clientArray.clients === undefined ? 
     <h3> Seems you have no clients, please add some! </h3> :
     <h3>  Please wait for your clients to load </h3>
   }
