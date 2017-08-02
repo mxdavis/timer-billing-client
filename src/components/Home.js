463eef79015ebb449fd1233501dd99d2fe3f51c1
mxdavis <md01@me.com>
2017-08-02 14:05:07 +0300

    change to stateless component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Home extends Component {
-  render(){
-    return(
-        <div className='uk-position-medium uk-position-center uk-text-center'>
-           <h1>Welcome to the Billing App</h1>
-           <h2>Login with Github</h2>
-            {/*render login page or write login, and have an image showing the login page...*/}
-           <h3> Open sourced billing app for developers by developers</h3>
-             <p>Create clients, projects, and tasks, and bill your clients properly</p>
-             <p> This is an open sourced project, so if you would like to contribute please put go to our 
-               <a href="https://github.com/mxdavis/timer-billing-client" target="_blank" rel="noopener noreferrer"> Github Page</a></p>
-               <p>Created by <a href="https://github.com/mxdavis/" target="_blank" rel="noopener noreferrer">mxdavis</a> as a project at the Flatiron School</p>
-        </div>
-    )
-  }
+const Home = (props) => {
+  return(
+      <div className='uk-position-medium uk-position-center uk-text-center'>
+          <h1>Welcome to the Billing App</h1>
+          <h2>Login with Github</h2>
+          {/*render login page or write login, and have an image showing the login page...*/}
+          <h3> Open sourced billing app for developers by developers</h3>
+            <p>Create clients, projects, and tasks, and bill your clients properly</p>
+            <p> This is an open sourced project, so if you would like to contribute please put go to our 
+              <a href="https://github.com/mxdavis/timer-billing-client" target="_blank" rel="noopener noreferrer"> Github Page</a></p>
+              <p>Created by <a href="https://github.com/mxdavis/" target="_blank" rel="noopener noreferrer">mxdavis</a> as a project at the Flatiron School</p>
+      </div>
+  )
+
 }
+
+export default Home
