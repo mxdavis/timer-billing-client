76c75cca87f903b48c98a3f2f3e1f9aea88b40b2
mxdavis <md01@me.com>
2017-08-02 12:28:17 +0300

    now date working on updating state

--- src/redux/reducers/tasks/tasks.js
+++ src/redux/reducers/tasks/tasks.js
@@ -14,6 +14,7 @@ export default function tasks(state = {
         billed_tasks: billed_tasks
       });
      case 'ADD_UNBILLED_TASK':
+     debugger
       const tasksWithoutChangedTask = state.unbilled_tasks.filter(t => t.task_id !== action.task.task_id)
       const changedTask = Object.assign({}, action.task, {
         client: action.task.clientValue.label,
@@ -21,8 +22,8 @@ export default function tasks(state = {
         project: action.task.projectValue.label,
         project_id: action.task.projectValue.value,
         total: action.task.bill_rate * action.task.bill_time,
-        date: action.task.startDate})
-      const allTasksWithChanged = [...tasksWithoutChangedTask, changedTask]
+        date: action.task.startDate["_i"]})
+      const allTasksWithChanged = tasksWithoutChangedTask.concat(changedTask)
       return Object.assign({}, state, {unbilled_tasks: allTasksWithChanged})
     default:
       return state;
