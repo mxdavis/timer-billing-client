a8b0fd56f2c1126f2c9fe193802d7f9cb0b6082d
mxdavis <md01@me.com>
2017-08-02 18:41:46 +0300

    update github

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:3000/api/v1/users/1/';
+// export const API_URL = 'http://localhost:3000/api/v1/users/1/';
 
-// export const API_URL = 'https://billing-timer.herokuapp.com/api/v1/'
+export const API_URL = 'https://billing-timer.herokuapp.com/api/v1/users/1/'
