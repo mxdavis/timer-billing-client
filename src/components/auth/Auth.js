fab3a9573d19af3625ef2d384291a694536f1e1d
mxdavis <md01@me.com>
2017-08-02 15:35:23 +0300

    switch back to local

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -4,9 +4,7 @@ export default class Auth extends Component {
   render(){
     return(
         <div className='uk-position-medium uk-position-center uk-text-center'>
-          If Signed in redirect to home page
-
-          If not show login and signup
+          Hold on tight, signup is coming soon! For now you can just browse around and try things out :)
         </div>
     )
   }
