629429d2df1ea9b761c06130f8dddbb84891b37b
mxdavis <md01@me.com>
2017-08-02 14:04:15 +0300

    make action and reducer universal with dataType

--- src/redux/actions/fetchingData.js
+++ src/redux/actions/fetchingData.js
@@ -1,7 +1,7 @@
 export const stopFetchingData = (dataType) => {
-  return { type: `STOP_FETCHING_${dataType}` }
+  return { type: `STOP_FETCHING_${dataType}`, dataType }
 }
 
 export const startFetchingData = (dataType) => {
-  return { type: `START_FETCHING_${dataType}` }
+  return { type: `START_FETCHING_${dataType}`, dataType }
 }
--- src/redux/reducers/fetchingData.js
+++ src/redux/reducers/fetchingData.js
@@ -1,17 +1,9 @@
 export default (state = [], action) => {
   switch(action.type) {
-    case "STOP_FETCHING_BILLED_TASKS":
-      return state.filter((i) => i !== "billed_tasks")
-    case "START_FETCHING_BILLED_TASKS":
-      return state.concat("billed_tasks")
-    case "STOP_FETCHING_UNBILLED_TASKS":
-      return state.filter((i) => i !== "unbilled_tasks")
-    case "START_FETCHING_UNBILLED_TASKS":
-      return state.concat("unbilled_tasks")
-    case "START_FETCHING_CLIENTS":
-      return state.concat("clients")
-    case "STOP_FETCHING_CLIENTS":
-      return state.filter((i) => i !== "clients")
+    case `STOP_FETCHING_${action.dataType}`:
+      return state.filter((i) => i !== action.dataType.toLocaleLowerCase())
+    case `START_FETCHING_${action.dataType}`:
+      return state.concat(action.dataType.toLocaleLowerCase())
     default: 
       return state;
   }
