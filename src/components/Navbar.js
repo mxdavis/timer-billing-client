ffe6c2aafcb788b1246b0d876ab6a2a408fdc5e7
mxdavis <md01@me.com>
2017-08-02 12:41:37 +0300

    move navbar to stateless component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom'
 import '../styles/navbar.css';
 
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div className="App">
-        <nav className="uk-navbar">
-          <div className="uk-navbar-flip">
-            <ul className="uk-navbar-nav">
-              <li><NavLink to="/">Home</NavLink></li>
-              <li><NavLink to="/signin">Sign In</NavLink></li>
-              <li><NavLink to="/">Add Task</NavLink></li>
-              <li><NavLink to="/clients/new">Add Client</NavLink></li>
-            </ul>
-          </div>
-        </nav>  
-      </div>
-    )
-  }
+const Navbar = () =>  {
+  return (
+    <div className="App">
+      <nav className="uk-navbar fixed">
+        <div className="uk-navbar-flip">
+          <ul className="uk-navbar-nav">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/signin">Sign In</NavLink></li>
+            <li><NavLink to="/tasks/new">Add Task</NavLink></li>
+            <li><NavLink to="/clients">Clients</NavLink></li>
+            <li><NavLink to="/tasks">Tasks</NavLink></li>
+          </ul>
+        </div>
+      </nav>  
+    </div>
+  )
 }
+
 
 export default Navbar;
