79701936f8b700ff18c70f92cf81bb217dc01150
mxdavis <md01@me.com>
2017-08-03 01:11:22 +0300

    update home page with auth component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import Auth from './auth/Auth'
+
 const Home = (props) => {
   return(
-      <div className='uk-position-medium uk-position-center uk-text-center'>
-          <h1>Welcome to the Billing App</h1>
-          <h2>Login with Github</h2>
-          {/*render login page or write login, and have an image showing the login page...*/}
-          <h3> Open sourced billing app for developers by developers</h3>
-            <p>Create clients, projects, and tasks, and bill your clients properly</p>
-            <p> This is an open sourced project, so if you would like to contribute please put go to our 
-              <a href="https://github.com/mxdavis/timer-billing-client" target="_blank" rel="noopener noreferrer"> Github Page</a></p>
-              <p>Created by <a href="https://github.com/mxdavis/" target="_blank" rel="noopener noreferrer">mxdavis</a> as a project at the Flatiron School</p>
+    <div className='uk-position-medium uk-position-center uk-text-center'>
+      <h1 >Welcome to the Billing App</h1>
+      <div className="padding-bottom">
+        <h3> Open sourced billing app for developers by developers</h3>
+      </div>
+      <div className="padding-bottom">
+        <Auth />
+      </div>
+      <div className="padding-top">
+        <p className="padding-top">Create clients, projects, and tasks, and bill your clients properly</p>
+        <p> This is an open sourced project, so if you would like to contribute please put go to our 
+        <a href="https://github.com/mxdavis/timer-billing-client" target="_blank" rel="noopener noreferrer"> Github Page</a></p>
+        <p>Created by <a href="https://github.com/mxdavis/" target="_blank" rel="noopener noreferrer">mxdavis</a> as a project at the Flatiron School</p></div>
       </div>
   )
 
