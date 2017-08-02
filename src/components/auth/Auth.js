6b0f10a7e2bdac2501e072a4419508158d757dc6
mxdavis <md01@me.com>
2017-08-02 14:05:17 +0300

    Create Auth

---
+++ src/components/auth/Auth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Auth extends Component {
+  render(){
+    return(
+        <div className='uk-position-medium uk-position-center uk-text-center'>
+          If Signed in redirect to home page
+
+          If not show login and signup
+        </div>
+    )
+  }
+}
