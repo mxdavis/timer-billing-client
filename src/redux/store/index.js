e3021fc0470e23fc126622bef738f1fa6c898f40
mxdavis <md01@me.com>
2017-08-03 00:43:51 +0300

    remove blank file from store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,12 +2,11 @@ import {createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 
 import clients from '../reducers/clients/clients'
-import addTask from '../reducers/tasks/addTask'
 import tasks from '../reducers/tasks/tasks'
 import fetchingData from '../reducers/fetchingData'
 
 const reducers = combineReducers({
-  tasks, addTask, clients, fetchingData
+  tasks, clients, fetchingData
 })
 
 const middleware = [thunk]
